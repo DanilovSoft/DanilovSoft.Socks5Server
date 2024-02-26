4115ccf6fc7b772d230cd6face947795b0f01faf
Vitalii Danilov <vitaliy931@gmail.com>
2024-02-26 21:26:51 +0200

    stash

--- SOCKS5Server/build_image.bat
+++ SOCKS5Server/build_image.bat
@@ -23,9 +23,9 @@ REM Создать виртуальный свитч на определенный интерфейс:
 docker network create -d transparent -o com.docker.network.windowsshim.interface="External" tlan
 
 REM Запуск контейнера:
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:01 --restart=always --name socks1 --hostname socks1 socks5server
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:02 --restart=always --name socks2 --hostname socks2 socks5server
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:03 --restart=always --name socks3 --hostname socks3 socks5server
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:04 --restart=always --name socks4 --hostname socks4 socks5server
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:05 --restart=always --name socks5 --hostname socks5 socks5server
-docker run -d --network=tlan --mac-address 00:15:5D:3D:30:06 --restart=always --name socks6 --hostname socks6 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:01 --restart=always --name socks1 --hostname socks1 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:02 --restart=always --name socks2 --hostname socks2 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:03 --restart=always --name socks3 --hostname socks3 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:04 --restart=always --name socks4 --hostname socks4 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:05 --restart=always --name socks5 --hostname socks5 socks5server
+docker run -d --network=tlan --mac-address 00:15:5D:3D:31:06 --restart=always --name socks6 --hostname socks6 socks5server
